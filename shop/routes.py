34d63b71fb824102da1cae606ec62c08af1b94dc
Donatas Noreika <donoras@gmail.com>
2022-04-07 11:32:56 +0300

    Added customers.html, orders.html, products.html

--- shop/routes.py
+++ shop/routes.py
@@ -1,6 +1,25 @@
 from shop import app
 from flask import render_template
+from .models import (Customer,
+                     MyOrder,
+                     Product)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/customers')
+def customers():
+    return render_template('customers.html', customers=Customer.query.all())
+
+
+@app.route('/orders')
+def orders():
+    return render_template('orders.html', orders=MyOrder.query.all())
+
+
+@app.route('/products')
+def products():
+    return render_template('products.html', products=Product.query.all())
